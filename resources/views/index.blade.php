08d432f69846e267da0d5443c020526c9661c6c1
rawan-mortaja <rawanmortaja077@gmail.com>
2023-07-21 00:08:41 +0300

    rawan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,57 +2,63 @@
 <html>
 
 <head>
-    <title>File Sharing App</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    @yield('styles')
+    <title>File Sharing Service</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
 </head>
 
 <body>
-    @if (session('success'))
-        <div>
-            {{ session('success') }}
-        </div>
-    @endif
+    <div class="container">
+        @if (session('success'))
+            <div>
+                {{ session('success') }}
+            </div>
+        @endif
 
-    @if (session('error'))
-        <div>
-            {{ session('error') }}
-        </div>
-    @endif
-    <form action="{{ route('file.upload') }}" method="post" enctype="multipart/form-data">
-        @csrf
-        <div class="form-group">
-            <input type="file" name="file">
-        </div>
-        <div class="form-group">
+        @if (session('error'))
+            <div>
+                {{ session('error') }}
+            </div>
+        @endif
+        <h1>File Sharing Service</h1>
+        <form action="{{ route('file.upload') }}" method="post" enctype="multipart/form-data">
+            @csrf
 
-            <button type="submit">Upload</button>
-        </div>
-    </form>
+            <div class="input-group">
+                <input type="file" class="form-control" id="inputGroupFile04" name="file"
+                    aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                <button class="btn btn-outline-secondary" type="submit" type="button"
+                    id="inputGroupFileAddon04">Upload</button>
+            </div>
+        </form>
 
-    <div class="row">
-        <div class="col-xs-8 col-xs-offset-2">
-            <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>File Name</th>
-                        <th>View</th>
-                        <th>Download</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $i = 1; ?>
-                    @foreach ($files as $file)
+        <div class="row">
+            <div class="col-xs-8 col-xs-offset-2">
+                <table class="table table-striped table-hover">
+                    <thead>
                         <tr>
-                            <td>{{ $i++ }}</td>
-                            <td>{{ $file->file_name }}</td>
-                            <td><a href="file.upload/{{ $file->file_name }}" target="_blank">View</a></td>
-                            <td><a href="file.download/{{ $file->file_name }}" download>Download</td>
+                            <th>#</th>
+                            <th>File Name</th>
+                            <th>Download</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php $i = 1; ?>
+                        @foreach ($files as $file)
+                            <tr>
+                                <td>{{ $i++ }}</td>
+                                <td>{{ $file->file_name }}</td>
+                                <td><a href="file.download/{{ $file->file_name }}" download>Download</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </body>
